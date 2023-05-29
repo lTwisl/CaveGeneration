960363190e85558f038ca0d7cb5696c9fb07be0a
Fedoseev Viacheslav <lslava121001l@gmail.com>
2023-05-29 22:53:39 +0300

    Добавил генерацию проходов между изолироваными участками.

--- Assets/MapGeneration.cs
+++ Assets/MapGeneration.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class MapGeneration : MonoBehaviour
 {
@@ -34,14 +36,14 @@ public class MapGeneration : MonoBehaviour
             SmoothMap();
         }
 
-        RemoveSmallItems();
+        ProcessingMap();
     }
 
     void RandomFillMap()
     {
         if (useRandomSeed)
             seed = Time.time.ToString().GetHashCode();
-        Random.InitState(seed);
+        System.Random rnd = new System.Random(seed);
 
         for (int y = 0; y < height; y++)
         {
@@ -52,8 +54,8 @@ public class MapGeneration : MonoBehaviour
                     map[y, x] = 1;
                     continue;
                 }
-
-                map[y, x] = Random.Range(0.0f, 1.0f) < percentFill / 100f ? 1 : 0;
+                
+                map[y, x] = rnd.Next(0, 100) < percentFill ? 1 : 0;
             }
         }
     }
@@ -78,23 +80,9 @@ public class MapGeneration : MonoBehaviour
         }
     }
 
-    void RemoveSmallItems()
+    void ProcessingMap()
     {
-        List<List<Cell>> regions = GetRegions(0);
-        foreach (List<Cell> region in regions)
-        {
-            int sizeRegion = region.Count;
-
-            if (sizeRegion > minSizeRoom)
-                continue;
-
-            foreach (Cell cell in region)
-            {
-                map[cell.y, cell.x] = 1;
-            }
-        }
-
-        regions = GetRegions(1);
+        List<List<Cell>> regions = GetRegions(1);
         foreach (List<Cell> region in regions)
         {
             int sizeRegion = region.Count;
@@ -107,6 +95,136 @@ public class MapGeneration : MonoBehaviour
                 map[cell.y, cell.x] = 0;
             }
         }
+
+        List<Room> survivingRooms = new List<Room>();
+        regions = GetRegions(0);
+        foreach (List<Cell> region in regions)
+        {
+            int sizeRegion = region.Count;
+
+            if (sizeRegion > minSizeRoom)
+            {
+                survivingRooms.Add(new Room(region, map));
+                continue;
+            }
+
+            foreach (Cell cell in region)
+            {
+                map[cell.y, cell.x] = 1;
+            }
+        }
+
+        survivingRooms.Sort();
+        survivingRooms[0].isAccessibleFromMainRoom = true;
+        survivingRooms[0].isMainRoom = true;
+
+        ConnectClosestRooms(survivingRooms);
+    }
+
+    void ConnectClosestRooms(List<Room> rooms)
+    {
+        Room mainRoom = rooms[0];
+        foreach (Room room in rooms)
+        {
+            if (room.isMainRoom)
+                continue;
+
+            room.distToMain = room.GetDistToRoom(mainRoom);
+        }
+
+        List<Room> sorted = rooms.OrderBy(r => r.distToMain).ToList();
+
+        foreach (Room roomA in sorted)
+        {
+            if (roomA.isMainRoom)
+                continue;
+
+            float bestDist = float.MaxValue;
+            Cell bestCellA = new Cell();
+            Cell bestCellB = new Cell();
+            Room bestRoomA = new Room();
+            Room bestRoomB = new Room();
+
+            foreach (Room roomB in rooms)
+            {
+                if (roomA == roomB)
+                    continue;
+
+                if (!roomB.isAccessibleFromMainRoom)
+                    continue;
+
+                float distBeteeenRooms = roomA.GetDistToRoom(roomB, out Cell tempA, out Cell tempB);
+
+                if (distBeteeenRooms < bestDist)
+                {
+                    bestDist = distBeteeenRooms;
+                    bestCellA = tempA;
+                    bestCellB = tempB;
+                    bestRoomA = roomA;
+                    bestRoomB = roomB;
+                }
+            }
+
+            CreatePassage(bestRoomA, bestRoomB, bestCellA, bestCellB);
+        }
+    }
+
+    void CreatePassage(Room roomA, Room roomB, Cell cellA, Cell cellB)
+    {
+        roomA.ConnectRoom(roomB);
+
+        int dx = cellB.x - cellA.x;
+        int dy = cellB.y - cellA.y;
+
+        if (Math.Abs(dx) > Math.Abs(dy))
+        {
+            int delta = System.Math.Sign(dx);
+            int x = cellA.x - delta;
+            
+            while (x != cellB.x)
+            {
+                x += delta;
+                int y = (int)((float)dy / dx * x - (float)dy / dx * cellA.x + cellA.y);
+
+                for (int y1 = y - 1; y1 <= y + 1; y1++)
+                {
+                    for (int x1 = x - 1; x1 <= x + 1; x1++)
+                    {
+                        if (!IsInMap(x1, y1))
+                            continue;
+
+                        map[y1, x1] = 0;
+                    }
+                }
+            }
+        }
+        else
+        {
+            int delta = System.Math.Sign(dy);
+            int y = cellA.y - delta;
+
+            while (y != cellB.y)
+            {
+                y += delta;
+                int x = (int)((float)dx / dy * y - (float)dx / dy * cellA.y + cellA.x);
+
+                for (int y1 = y-1; y1 <= y+1; y1++)
+                {
+                    for (int x1 = x - 1; x1 <= x + 1; x1++)
+                    {
+                        if (!IsInMap(x1, y1))
+                            continue;
+
+                        map[y1, x1] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    Vector3 CellToWorldPoint(Cell cell)
+    {
+        return new Vector3(cell.x - width / 2, cell.y - height / 2, -5);
     }
 
     int GetCountNeighbors(int x, int y)
@@ -230,6 +348,27 @@ public class MapGeneration : MonoBehaviour
 
             }
         }
+
+        /*List<List<Cell>> regions = GetRegions(0);
+        foreach (List<Cell> region in regions)
+        {
+            Room room = new Room(region, map);
+            //Debug.Log(room.edgeCells.Count);
+            for (int i = 0; i < room.edgeCells.Count; i++)
+            {
+                int x = room.edgeCells[i].x;
+                int y = room.edgeCells[i].y;
+                Gizmos.color = Color.blue;
+                Vector3 pos = new Vector3(x - width / 2f, y - height / 2f, 0);
+                Gizmos.DrawCube(pos, Vector3.one);
+            }
+        }
+
+        Vector3 pos1 = CellToWorldPoint(new Cell(0, 0));
+        Gizmos.DrawSphere(pos1, 0.5f);
+
+        Vector3 pos2 = CellToWorldPoint(new Cell(1, 0));
+        Gizmos.DrawSphere(pos2, 0.5f);*/
     }
 
     struct Cell
@@ -241,6 +380,134 @@ public class MapGeneration : MonoBehaviour
         {
             this.x = x;
             this.y = y;
+        }
+    }
+
+    class Room : System.IComparable<Room>
+    {
+        public List<Cell> cells;
+        public List<Cell> edgeCells;
+        public int sizeRoom;
+
+        public List<Room> connectedRooms;
+
+        public bool isAccessibleFromMainRoom;
+        public bool isMainRoom;
+
+        public float distToMain;
+
+        public Room() { }
+
+        public Room(List<Cell> cells, int[,] map)
+        {
+            this.cells = cells;
+            sizeRoom = cells.Count;
+            connectedRooms = new List<Room>();
+
+            edgeCells = new List<Cell>();
+            foreach (Cell cell in cells)
+            {
+                bool founed = false;
+                for (int neighborsY = cell.y - 1; neighborsY <= cell.y + 1; neighborsY++)
+                {
+                    for (int neighborsX = cell.x - 1; neighborsX <= cell.x + 1; neighborsX++)
+                    {
+                        if (neighborsX == cell.x && neighborsY == cell.y) // —омого себ€ провер€ть не надо
+                            continue;
+
+                        if (neighborsX != cell.x && neighborsY != cell.y) // ƒиагональные €чейки провер€ть не надо
+                            continue;
+
+                        if (map[neighborsY, neighborsX] == 0)
+                            continue;
+
+                        edgeCells.Add(cell);
+                        founed = true;
+                    }
+                    if (founed)
+                        break;
+                }
+            }
+        }
+
+        
+        public void SetAccessibleFromMainRoom()
+        {
+            if (isAccessibleFromMainRoom)
+                return;
+
+            isAccessibleFromMainRoom = true;
+            foreach (Room room in connectedRooms)
+            {
+                room.SetAccessibleFromMainRoom();
+            }
+        }
+
+        public void ConnectRoom(Room otherRoom)
+        {
+            if (isAccessibleFromMainRoom)
+            {
+                otherRoom.SetAccessibleFromMainRoom();
+            }
+            else if (otherRoom.isAccessibleFromMainRoom)
+            {
+                SetAccessibleFromMainRoom();
+            }
+
+            connectedRooms.Add(otherRoom);
+            otherRoom.connectedRooms.Add(this);
+        }
+
+        public bool IsConnected(Room otherRoom)
+        {
+            return connectedRooms.Contains(otherRoom);
+        }
+
+        public float GetDistToRoom(Room other)
+        {
+            float bestDist = float.MaxValue;
+            foreach (Cell cellA in edgeCells)
+            {
+                foreach (Cell cellB in other.edgeCells)
+                {
+                    float distBeteeenRooms = Mathf.Pow(cellA.x - cellB.x, 2) + Mathf.Pow(cellA.y - cellB.y, 2);
+
+                    if (distBeteeenRooms < bestDist)
+                    {
+                        bestDist = distBeteeenRooms;
+                    }
+                }
+            }
+
+            return bestDist;
+        }
+
+        public float GetDistToRoom(Room other, out Cell bestCellA, out Cell bestCellB)
+        {
+            float bestDist = float.MaxValue;
+            bestCellA = new Cell();
+            bestCellB = new Cell();
+            foreach (Cell cellA in edgeCells)
+            {
+                foreach (Cell cellB in other.edgeCells)
+                {
+                    float distBeteeenRooms = Mathf.Pow(cellA.x - cellB.x, 2) + Mathf.Pow(cellA.y - cellB.y, 2);
+
+                    if (distBeteeenRooms < bestDist)
+                    {
+                        bestDist = distBeteeenRooms;
+                        bestCellA = cellA;
+                        bestCellB = cellB;
+                    }
+                }
+            }
+
+            return bestDist;
+        }
+
+        public int CompareTo(Room other)
+        {
+            return sizeRoom.CompareTo(other.sizeRoom);
         }
     }
 }
